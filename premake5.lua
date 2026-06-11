e24cf931c5edc37dcb0d42c59e05a4c13cba3d0f
Gabriel Piazza <contact@gpiazza.me>
2026-06-11 17:34:49 -0400

    Added ability to modify all components on hierarchy multiselection

--- premake5.lua
+++ premake5.lua
@@ -20,7 +20,8 @@ project "ImGui"
 		"imstb_textedit.h",
 		"imstb_truetype.h",
 		"imgui_demo.cpp",
-        "imgui_tables.cpp"
+        "imgui_tables.cpp",
+		"misc/cpp/imgui_stdlib.cpp"
 	}
 
 	filter "system:windows"
